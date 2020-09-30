b1d0c60e78a498ca048f6aa73b01f78c88cb4d8e
Ali <mohammadali18997@gmail.com>
2020-09-29 20:20:00 -0400

    Imports and Message

--- Gossip/Gossip/Program.fs
+++ Gossip/Gossip/Program.fs
@@ -1,6 +1,17 @@
-﻿// Learn more about F# at http://fsharp.org
-
+﻿//Imports
+open Akka
+open Akka.FSharp
 open System
+open System.Diagnostics
+open Akka.Actor
+
+type ChildMessage() = 
+
+    //starting number of subproblem
+    [<DefaultValue>] val mutable start: int
+
+    //length of subproblem
+    [<DefaultValue>] val mutable length: int
 
 [<EntryPoint>]
 let main argv =
