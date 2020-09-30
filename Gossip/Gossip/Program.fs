4c891a3eabd447b196c739dd261bd6dc4a454027
Ali <mohammadali18997@gmail.com>
2020-09-29 20:31:20 -0400

    Working Actor

--- Gossip/Gossip/Program.fs
+++ Gossip/Gossip/Program.fs
@@ -5,15 +5,49 @@ open System
 open System.Diagnostics
 open Akka.Actor
 
-type ChildMessage() = 
+//Create System reference
+let system = System.create "system" <| Configuration.defaultConfig()
 
-    //starting number of subproblem
-    [<DefaultValue>] val mutable start: int
+type Message() = 
+    [<DefaultValue>] val mutable s: int
+    [<DefaultValue>] val mutable w: int
 
-    //length of subproblem
-    [<DefaultValue>] val mutable length: int
+
+//Actor
+let actor (actorMailbox:Actor<Message>) = 
+
+    //Actor Loop that will process a message on each iteration
+    let rec actorLoop() = actor {
+
+        //Receive the message
+        let! msg = actorMailbox.Receive()
+        printf "%A" msg.s
+        printf "%A" msg.w
+
+        return! actorLoop()
+    }
+
+    //Call to start the actor loop
+    actorLoop()
+
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
+let main(args) =
+
+    //Create the starting message
+    let startMessage = new Message()
+
+    //Add values to the message
+    startMessage.s <- 0
+    startMessage.w <- 0
+
+    //Create the start actor
+    let startActor = spawn system "start" actor
+   
+    //And pass it the start message
+    startActor <! startMessage
+
+    //Keep the console open by making it wait for key press
+    System.Console.ReadKey() |> ignore
+
     0 // return an integer exit code
